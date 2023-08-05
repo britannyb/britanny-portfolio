36d616458aff564b9de56fde4eab4b6006e6800f
britannyb <britbaldovino@gmail.com>
2023-08-05 11:29:54 +0800

    fixed navigation

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -6,17 +6,14 @@ import SkillsIcon from "./SkillsIcon";
 import AboutIcon from "./AboutIcon";
 
 const BottomNavigation = () => {
-  const [themeToggle, setThemeToggle] = useState(true);
   const toggleTheme = () => {
     if (localStorage.getItem("color-theme")) {
       if (localStorage.getItem("color-theme") === "light") {
         document.documentElement.classList.add("dark");
         localStorage.setItem("color-theme", "dark");
-        setThemeToggle(false);
       } else {
         document.documentElement.classList.remove("dark");
         localStorage.setItem("color-theme", "light");
-        setThemeToggle(true);
       }
     } else {
       if (document.documentElement.classList.contains("dark")) {
@@ -45,11 +42,12 @@ const BottomNavigation = () => {
         </div>
         <div id="toggle-button">
           <button id="theme-toggle" type="button" onClick={toggleTheme}>
-            {themeToggle ? (
+            <div className="inline dark:hidden">
               <div id="dark-toggle"></div>
-            ) : (
+            </div>
+            <div className="hidden dark:inline">
               <div id="light-toggle"></div>
-            )}
+            </div>
           </button>
         </div>
       </div>
