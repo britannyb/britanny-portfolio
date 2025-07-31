99caafc026c41db6e546df01c3d41818c52b1df3
britannyb <britbaldovino@gmail.com>
2025-07-31 19:39:50 +0800

    resized misc content

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -290,7 +290,7 @@ const Projects = () => {
             <ContainerContent title="Sample Drawings (Procreate)">
               <div
                 id="drawing-gallery"
-                className="grid grid-cols-1 md:grid-cols-3 gap-2 items-center centered-image pb-4"
+                className="lg:w-[95%] grid grid-cols-1 md:grid-cols-3 gap-2 items-center centered-image pb-4"
               >
                 <DrawingModal
                   imageUrl="images/drawing1.jpg"
@@ -310,7 +310,7 @@ const Projects = () => {
             <ContainerContent title="Toy Photography">
               <div
                 id="toy-gallery"
-                className="grid grid-cols-1 md:grid-cols-3 gap-2 rounded-images text-xsm lg:text-sm pb-4"
+                className="lg:w-[95%] grid grid-cols-1 md:grid-cols-3 gap-2 rounded-images text-xsm lg:text-sm pb-4"
               >
                 <ImageModal
                   imageUrl="images/toy1.jpg"
