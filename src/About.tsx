317015f3218d0f80a01652c7c5db59bb63b97bc4
britannyb <britbaldovino@gmail.com>
2025-08-01 15:01:31 +0800

    added about info

--- src/About.tsx
+++ src/About.tsx
@@ -33,7 +33,8 @@ const About = () => {
   return (
     <div
       id="about"
-      className="about-bg dark:about-bg-dark main-container pb-14 md:pb-0">
+      className="about-bg dark:about-bg-dark main-container pb-14 md:pb-0"
+    >
       <div className="content text-white">
         <Container title="ABOUT">
           <div className="lg:flex lg:space-x-11 items-center">
@@ -64,9 +65,10 @@ const About = () => {
               <AboutContainer iconType="heart">
                 <p>
                   My previous jobs include being a web developer and web
-                  designer. In relation, my interests mainly include
-                  programming, designing, and drawing. I also love playing video
-                  games.
+                  designer. I have also been a college instructor and taught
+                  subjects such as "Web Technologies", "Data Analytics", and
+                  others. My interests mainly include programming, designing,
+                  and drawing. I also love playing video games.
                 </p>
               </AboutContainer>
               <br />
@@ -77,7 +79,8 @@ const About = () => {
                     <a
                       className="text-white hover:text-pink flex items-center"
                       href="https://www.behance.net/britannyb"
-                      target="_blank">
+                      target="_blank"
+                    >
                       <img
                         src="images/behanceLogo.png"
                         alt="Behance Icon"
@@ -92,7 +95,8 @@ const About = () => {
                     <a
                       className="text-white hover:text-pink flex items-center"
                       href="https://rive.app/@britannyb/"
-                      target="_blank">
+                      target="_blank"
+                    >
                       <img
                         src="images/riveLogo.png"
                         alt="Rive Icon"
@@ -117,7 +121,8 @@ const About = () => {
                 <a
                   href="https://www.pexels.com/@mo-eid-1268975/"
                   target="_blank"
-                  className="text-white break-words hover:text-pink">
+                  className="text-white break-words hover:text-pink"
+                >
                   <h4 className="text-sm md:text-md">
                     Mo Eid @ Pexels
                     <StyledPointer />
@@ -129,7 +134,8 @@ const About = () => {
                 <a
                   href="https://www.iconfinder.com/iconsets/line-basic-ecommerce"
                   target="_blank"
-                  className="text-white break-words hover:text-pink">
+                  className="text-white break-words hover:text-pink"
+                >
                   <h4 className="text-sm md:text-md">
                     Nikita Grachev @ Iconfinder
                     <StyledPointer />
