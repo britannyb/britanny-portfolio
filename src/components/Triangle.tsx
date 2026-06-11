9fa1e810949acd4a92ba64c61395a2e4599074df
britannyb <britbaldovino@gmail.com>
2026-06-11 13:12:05 +0800

    improved next icons

--- src/components/Triangle.tsx
+++ src/components/Triangle.tsx
@@ -3,24 +3,12 @@ const Triangle = () => {
     <svg
       width="33"
       height="33"
-      viewBox="0 0 96 96"
+      viewBox="0 0 33 33"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="pr-1"
+      className="scale-90 hover:scale-110"
     >
-      <g>
-        <path
-          d="M39.3756,48.0022l30.47-25.39a6.0035,6.0035,0,0,0-7.6878-9.223L26.1563,43.3906a6.0092,6.0092,0,0,0,0,9.2231L62.1578,82.615a6.0035,6.0035,0,0,0,7.6878-9.2231Z"
-          fill="white"
-        />
-        <path
-          d="M39.3756,48.0022l30.47-25.39a6.0035,6.0035,0,0,0-7.6878-9.223L26.1563,43.3906a6.0092,6.0092,0,0,0,0,9.2231L62.1578,82.615a6.0035,6.0035,0,0,0,7.6878-9.2231Z"
-          className="stroke-white"
-          strokeMiterlimit="10"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </g>
+      <path d="M20 28L9.5 16L20 5" stroke="white" strokeWidth="4" />
     </svg>
   );
 };
